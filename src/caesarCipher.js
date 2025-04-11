4c678c087a81e9969bf04a8e1858a67c7f509657
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-04-11 09:52:23 +0100

    feat: Create caeserCipher function, changes characters in a string using a shifter

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,5 +1,52 @@
 const caesarCipher = (string, shift) => {
-  return z;
-}
+  const originalString = string.split('');
+  const alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  const newString = originalString.map((char) => {
+    if (alphabet.includes(char.toLowerCase())) {
+      const charIndex = alphabet.indexOf(char.toLowerCase());
+      let newIndex = (charIndex + shift) % 26;
+      if (newIndex < 0) {
+        newIndex = 26 + newIndex;
+      }
+      else if (newIndex > 25) {
+        newIndex = newIndex - 26;
+      }
+      let newChar;
+      char === char.toUpperCase() ? newChar = alphabet[newIndex].toUpperCase() : newChar = alphabet[newIndex];
+      return newChar;
+    }
+    return char;
+  });
+
+  return newString.join('');
+};
 
 module.exports = caesarCipher;
+1
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,7 +1,25 @@
 const caesar = require('../src/caesarCipher');
 
 describe('#caesarCipher', () => {
+  it('should output a', () => {
+    expect(caesar('a', 0)).toEqual('a');
+  })
+  it('should output z', () => {
+    expect(caesar('z', 0)).toEqual('z');
+  })
+  it('should output a', () => {
+    expect(caesar('z', 1)).toEqual('a');
+  })
   it('should output z', () => {
     expect(caesar('a', -1)).toEqual('z');
+  })
+  it('should output b', () => {
+    expect(caesar('z', 2)).toEqual('b');
+  })
+  it('should output Khoor, Zruog!', () => {
+    expect(caesar('Hello, World!', 3)).toEqual('Khoor, Zruog!');
+  })
+  it('should output MARCUS CRASSUS!', () => {
+    expect(caesar('RFWHZX HWFXXZX!', -5)).toEqual('MARCUS CRASSUS!');
   })
 })
