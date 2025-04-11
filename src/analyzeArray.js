5cc21676ef8c6cf672d390adcaf40f934a0ccd74
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-04-11 10:19:52 +0100

    feat: Return array's min, max and length in an object

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,9 +1,25 @@
 const analyzeArray = (array) => {
+  if (array.length === 0) {
+    return {
+      average: 0,
+      min: 0,
+      max: 0,
+      length: 0,
+    }
+  }
   const sum = array.reduce((prev, sum) => {
     return sum += prev;
   }, 0)
 
-  return sum;
+  const min = array.sort()[0];
+  const max = array.sort((a, b) => b - a)[0];
+
+  return {
+    average: (sum / array.length),
+    min,
+    max,
+    length: array.length,
+  };
 }
 
 module.exports = analyzeArray;
--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -1,9 +1,36 @@
 const analyzeArray = require('../src/analyzeArray');
 
 describe(('#analyzeArray'), () => {
-  describe(('#average'), () = > {
+  describe(('#average'), () => {
     it('should output 1', () => {
       expect(analyzeArray([1]).average).toBe(1);
     })
+    it('should output 4', () => {
+      expect(analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe(4);
+    })
+    it('should output 0', () => {
+      expect(analyzeArray([]).average).toBe(0);
+    })
   })
+  
+  describe(('#min'), () => {
+    it('should output 2', () => {
+      expect(analyzeArray([2, 4]).min).toBe(2);
+    })
+    it('should output 1', () => {
+      expect(analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe(1);
+    })
+  })
+
+  describe(('#max'), () => {
+    it('should output 8', () => {
+      expect(analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe(8);
+    }) 
+  });
+
+  describe(('#length'), () => {
+    it('should output 6', () => {
+      expect(analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe(6);
+    }) 
+  });
 });
